de22a8f664e65aa630ebc58b3bf4fc119feadeb9
TapiocaQueen990 <daniel.phou@hotmail.com>
2024-04-07 12:59:58 -0400

    added feature that counts how many items and categories there are

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,6 +1,8 @@
 class Admin::DashboardController < ApplicationController
   before_action :authenticate_admin
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
 
   def authenticate_admin
