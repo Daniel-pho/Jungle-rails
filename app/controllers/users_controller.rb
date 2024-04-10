edd2588ec464b10117b9152981a090ad32bca6cc
TapiocaQueen990 <daniel.phou@hotmail.com>
2024-04-10 12:18:02 -0400

    added testing for users with rspec

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,10 @@
 class UsersController < ApplicationController
+  validates :email, presence: true
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :password, presence: true
+  validates :password_confirmation, presence: true
+  
   def new
     @user = User.new
   end
--- spec/models/user_spec_spec.rb
+++ spec/models/user_spec_spec.rb
@@ -2,8 +2,7 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   describe 'Validations' do
-    # Example for presence validation of required fields
-    it 'should validate presence of email, first name, last name, password, and password_confirmation' do
+    it 'requires email, first_name, last_name, password, and password_confirmation' do
       user = User.new
       user.valid?
       expect(user.errors.full_messages).to include(
@@ -14,34 +13,46 @@ RSpec.describe User, type: :model do
         "Password confirmation can't be blank"
       )
     end
-    it 'should validate that password matches password_confirmation' do
-      user = User.new(
-        email: 'test@test.com',
-        first_name: 'John',
-        last_name: 'Doe',
-        password: 'password',
-        password_confirmation: 'different_password'
-      )
-      user.valid?
+
+    it 'password and password_confirmation must match' do
+      user = User.new(password: 'password', password_confirmation: 'different_password')
+      expect(user).to_not be_valid
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
-    it 'should validate uniqueness of email' do
-      User.create(
-        email: 'test@test.com',
+
+    it 'requires a minimum password length' do
+      user = User.new(
+        email: 'test@example.com',
         first_name: 'John',
         last_name: 'Doe',
-        password: 'password',
-        password_confirmation: 'password'
-      )
-      user = User.new(
-        email: 'TEST@test.COM', 
-        first_name: 'Jane',
-        last_name: 'Smith',
-        password: 'password',
-        password_confirmation: 'password'
+        password: '12345', 
+        password_confirmation: '12345'
       )
       user.valid?
-      expect(user.errors.full_messages).to include("Email has already been taken")
+      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
     end
+  end #describe validations
+
+  describe '.authenticate_with_credentials' do
+    before do
+      @user = User.create(email: 'test@example.com',
+      first_name: 'John',
+      last_name: 'Doe',
+      password: 'password', 
+      password_confirmation: 'password')
+    end
+
+    context 'when email and password are correct' do
+      it 'returns the user' do
+        authenticated_user = User.authenticate_with_credentials('test@example.com', 'password')
+        expect(authenticated_user).to eq(@user)
+      end
+    end
+    
+    it 'ignores case sensitivity in email' do
+      authenticated_user = User.authenticate_with_credentials('tEsT@eXaMpLe.CoM', 'password')
+      expect(authenticated_user).to eq(@user)
+    end
+
   end
-end
+end # describe user
